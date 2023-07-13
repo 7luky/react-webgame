1c063a75378a3ac24e63cb3eda12bd2d1d39ce03
7luky <64417043+7luky@users.noreply.github.com>
2023-07-13 17:56:25 +0900

    first-project

---
+++ 2.끝말잇기/client.jsx
@@ -0,0 +1,7 @@
+const React = require("react");
+const ReactDOM = require("react-dom/client");
+
+const WordRelay = require("./WordRelay");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<WordRelay />);
